c8605f6ed7a303f69d889dc58ae0f58aca9c4c54
BuildTools <unconfigured@null.spigotmc.org>
2022-05-07 15:38:11 +0200

    removed spaces to comply with pep8

--- plot.py
+++ plot.py
@@ -1,7 +1,7 @@
 import plotly.graph_objects as go
 import pandas as pd
 
-df = pd.read_csv("points.csv", header=0, sep = ",")
+df = pd.read_csv("points.csv", header=0, sep=",")
 fig = go.Figure()
 fig.add_trace(go.Scatter(x=df["x"].to_list(), y=df["y"].to_list(), fill="toself"))
 fig.show()
